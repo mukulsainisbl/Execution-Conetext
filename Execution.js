a7caeedd505492c35a3bb41351f8df5735ef5196
mukulsaini <mukulsainisbl1234@gmail.com>
2025-06-18 15:05:57 +0530

    Add execution code

---
+++ Execution.js
@@ -0,0 +1,12 @@
+var age = 25;
+function displayAge() {
+  console.log("Display Age", age);
+}
+displayAge();
+
+function changeAge() {
+  age = 30;
+  console.log("Change Age", age);
+}
+
+changeAge();
